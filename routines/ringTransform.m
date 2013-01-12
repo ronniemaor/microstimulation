8d3d523921b4a9aaf22c7684cba288d6bf82da22
Ronnie Maor <ronnie@cloudshare.com>
2013-01-12 22:55:00 +0200

    some more work on the rings analysis (similar to changes made for the slices)

--- routines/ringTransform.m
+++ routines/ringTransform.m
@@ -1,5 +1,4 @@
-function [means, rVals] = ringTransform(frames,mask,W)
+function [means, rVals] = ringTransform(frames,mask,C,W)
 N = [100 100];
-C = [41 41]; % HARDCODED stimulation point
 R = rings(N,C,mask,W);
 [means,rVals] = mfEqMeans(frames,R);
--- snippets/ringsAnalysis.m
+++ snippets/ringsAnalysis.m
@@ -3,24 +3,39 @@ blank = mean(conds(:,:,5),3);
 meanFrame = mean(blank(:,2:100),2);
 blVes = mfilt2(meanFrame,100,100,2,'hm');
 mask = chamberMask(condsn);
-R = rings([100,100],[41,41],mask,3);
+C = [30,38];
+W = 3;
+R = rings([100,100],C,mask,W);
 blVes(R==10) = min(blVes);
 %blVes(maskPix) = min(blVes);
 figure; mimg(blVes,100,100); impixelinfo;
 
 %% surface plot after radius transform
-signal = relativeSignal(condsn);
+range = 2:230;
+signal = relativeSignal(condsn,range);
 mask = chamberMask(condsn);
-means = ringTransform(signal,mask,3);
-means = means(1:18,:);
-figure; surf(means); colorbar;
-set(gca,'XTick',[25, 50:50:200]);
-xlabel('Frame'); ylabel('Radius'); zlabel('Signal'); 
+W = 3;
+C = [30,38];
+[means,rVals] = ringTransform(signal,mask,C,W);
+mmPerPixel = 0.1;
+distances = rVals * W * mmPerPixel; % convert to mm
+msecPerFrame = 10;
+times = (range-25)*10; % convert to msec from onset
+figure; 
+surf(times,distances,means);
+view(0,90);
+colorbar;
+title('Signal strength for time and radius');
+xlabel('Time from stimulus onset (msec)'); 
+ylabel('Radius from peak (mm)'); 
+zlabel('Signal'); 
 
 %% signal vs. radius for several time points
 signal = relativeSignal(condsn);
 mask = chamberMask(condsn);
-means = ringTransform(signal,mask,3);
+W = 3;
+C = [30,38];
+means = ringTransform(signal,mask,C,W);
 %frames = 32:2:50; 
 frames = 24:2:32;
 means = means(1:18,frames);
@@ -31,7 +46,9 @@ legend(arraySprintf('Frame %d',frames))
 %% signal vs. time for several radiuses
 signal = relativeSignal(condsn);
 mask = chamberMask(condsn);
-means = ringTransform(signal,mask,3);
+W = 3;
+C = [30,38];
+means = ringTransform(signal,mask,C,W);
 radiuses = [0, 5, 10]; 
 %radiuses = 0:2:10;
 means = means(radiuses+1,:);
