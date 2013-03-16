3739f1320c9ca71dbfed5205d9ad2d408987ec71
Ronnie Maor <ronnie@cloudshare.com>
2013-03-16 12:50:42 +0200

    moved 3rd party to different directory. added barwitherr

--- snippets/setupPath.m
+++ snippets/setupPath.m
@@ -1,3 +1,5 @@
 path(path,'C:\data\zuta\My_M_Files\Matlab4Hamutal');
 path(path,'C:\data\zuta\My_M_Files\Matlab4Inbal');
+path(path,'C:\Users\ronnie\Google Drive\brain\my stuff\zuta\microstimulation\Ronnie-M-Files\snippets');
 path(path,'C:\Users\ronnie\Google Drive\brain\my stuff\zuta\microstimulation\Ronnie-M-Files\routines');
+path(path,'C:\Users\ronnie\Google Drive\brain\my stuff\zuta\microstimulation\Ronnie-M-Files\3rd party');
