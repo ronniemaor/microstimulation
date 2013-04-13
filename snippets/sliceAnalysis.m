f52e827bf392ae9b82b54c5f5af43812baf7fccb
Ronnie Maor <ronnie.maor@gmail.com>
2013-04-13 12:59:06 +0300

    single trials - model comparison (without debug prints)

--- snippets/sliceAnalysis.m
+++ snippets/sliceAnalysis.m
@@ -40,7 +40,7 @@ W = 9;
 nBins = 5;
 fits = {GaussianFit(1), ExponentialFit(1)};
 fitNames = cellfun(@(fit) fit.name(),fits, 'UniformOutput',false);
-%figure
+figure
 nFits = length(fits);
 
 nSlices = 2;
@@ -57,37 +57,37 @@ for iSlice = 1:nSlices
     distances = eqVals * mmPerPixel; % convert to mm
     eqSEM = sqrt(mean(eqStd.^2,1)/size(eqStd,1)); % estimate SEM over all trials
     
-    %subplot(2,2,iSlice)
-    %errorbar(distances, mean(eqMeans,1), eqSEM);
-    %hold on
-    %colors = get(gca,'ColorOrder');
+    subplot(2,2,iSlice)
+    errorbar(distances, mean(eqMeans,1), eqSEM);
+    hold on
+    colors = get(gca,'ColorOrder');
 
     for iFit = 1:nFits
         fit = fits{iFit};
         [yFit,P,err,errSem, overfitR2] = ...
-                   crossValidationRegression(fit,distances,eqMeans,nBins, strAxis);
+                   crossValidationRegression(fit,distances,eqMeans,nBins);
         errCases(iSlice,iFit) = err;
         errSemCases(iSlice,iFit) = errSem;
-        %plot(distances, yFit, 'Color', colors(iFit+1,:));
-        %title(sprintf('%s slice',strAxis));
-        %xlabel('Distance from peak center (mm)'); 
-        %ylabel('Relative signal');
+        plot(distances, yFit, 'Color', colors(iFit+1,:));
+        title(sprintf('%s slice',strAxis));
+        xlabel('Distance from peak center (mm)'); 
+        ylabel('Relative signal');
     end
-    %legend('Measured values', fitNames{:});
+    legend('Measured values', fitNames{:});
 end
 
-%subplot(2,2,[3 4])
-%barwitherr(errSemCases, errCases);
-%title('Goodness of fit for the different models');
-%set(gca,'XTickLabel',sliceNames)
-%ylabel('R2')
-%legend(fitNames,'Location','North')
+subplot(2,2,[3 4])
+barwitherr(errSemCases, errCases);
+title('Goodness of fit for the different models');
+set(gca,'XTickLabel',sliceNames)
+ylabel('R2')
+legend(fitNames,'Location','North')
 
-%topVals = errCases + errSemCases;
-%bottomVals = errCases - errSemCases;
-%ymax = max(topVals(:));
-%ymin = min(bottomVals(:));
-%ylim([max(0,ymin-0.2), min(1,ymax+0.05)])
+topVals = errCases + errSemCases;
+bottomVals = errCases - errSemCases;
+ymax = max(topVals(:));
+ymin = min(bottomVals(:));
+ylim([max(0,ymin-0.2), min(1,ymax+0.05)])
 
 if length(peakRange) > 1
     strFrames = sprintf('frames %d:%d', min(peakRange), max(peakRange));
@@ -95,7 +95,7 @@ else
     strFrames = sprintf('frame %d', peakRange);
 end
 t = sprintf('Fits for %s, W=%d, C=(%d,%d)', strFrames, W, C(1), C(2));
-%topLevelTitle(t);
+topLevelTitle(t);
 
 %% how fit parameters change over time
 frameRange = rangeFromWidth(peakFrame,11);
