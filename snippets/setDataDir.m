412e08fd10ca957a193293776348429d21a3a320
Ronnie Maor <ronnie.maor@gmail.com>
2013-04-19 07:54:13 +0300

    new data dirs

--- snippets/setDataDir.m
+++ snippets/setDataDir.m
@@ -3,9 +3,16 @@ if ~exist(baseDataDir,'dir')
     error('Could not find baseDataDir')
 end
 
-%sessionDir = 'Leg/leg_2009_01_29/c';
-sessionDir = 'Leg/leg_2009_01_29/c-with-noisy-files-fix';
-%sessionDir = 'Leg/leg_2009_04_01/d';
+sessionDir = 'Leg/leg_2009_01_29/c';
+% sessionDir = 'Leg/leg_2009_01_29/g';
+% sessionDir = 'Leg/leg_2009_01_29/i';
+
+% sessionDir = 'Leg/leg_2009_03_18/b';
+% sessionDir = 'Leg/leg_2009_03_18/c';
+% sessionDir = 'Leg/leg_2009_03_18/d';
+% sessionDir = 'Leg/leg_2009_03_18/e';
+
+% sessionDir = 'Leg/leg_2009_04_01/d';
 
 dataDir = [baseDataDir, '/', sessionDir];
 fprintf('Setting data dir to %s\n', dataDir)
