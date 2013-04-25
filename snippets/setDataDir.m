6bab8e84fda23d88073ea250f4b58ff34104a774
Ronnie Maor <ronnie.maor@gmail.com>
2013-04-25 08:06:13 +0300

    use containers.Map for session dirs

--- snippets/setDataDir.m
+++ snippets/setDataDir.m
@@ -3,17 +3,19 @@ if ~exist(baseDataDir,'dir')
     error('Could not find baseDataDir')
 end
 
-sessionDir = 'Leg/leg_2009_01_29/c';
-% sessionDir = 'Leg/leg_2009_01_29/g';
-% sessionDir = 'Leg/leg_2009_01_29/i';
-% sessionDir = 'Leg/leg_2009_01_29/j';
+allSessions = containers.Map();
+allSessions('J29c') = 'Leg/leg_2009_01_29/c';
+allSessions('J29g') = 'Leg/leg_2009_01_29/g';
+allSessions('J29i') = 'Leg/leg_2009_01_29/i';
+allSessions('J29j') = 'Leg/leg_2009_01_29/j';
+allSessions('M18b') = 'Leg/leg_2009_03_18/b';
+allSessions('M18c') = 'Leg/leg_2009_03_18/c';
+allSessions('M18d') = 'Leg/leg_2009_03_18/d';
+allSessions('M18e') = 'Leg/leg_2009_03_18/e';
+allSessions('A1d') = 'Leg/leg_2009_04_01/d';
 
-% sessionDir = 'Leg/leg_2009_03_18/b';
-% sessionDir = 'Leg/leg_2009_03_18/c';
-% sessionDir = 'Leg/leg_2009_03_18/d';
-% sessionDir = 'Leg/leg_2009_03_18/e';
+sessionKey = 'J29c';
 
-% sessionDir = 'Leg/leg_2009_04_01/d';
-
+sessionDir = allSessions(sessionKey);
 dataDir = [baseDataDir, '/', sessionDir];
-fprintf('Setting data dir to %s\n', dataDir)
+fprintf('Using session %s. Setting data dir to %s\n', sessionKey, dataDir)
