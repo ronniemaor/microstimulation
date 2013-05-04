bc7ee323667cf68581bdc5b0ec3132fff793edd3
Ronnie Maor <ronnie.maor@gmail.com>
2013-05-04 16:05:12 +0300

    mimg figure - added ability to normalize times to msec from stimulation start and not frame number

--- snippets/drawMimg.m
+++ snippets/drawMimg.m
@@ -1,16 +1,26 @@
 %% time series of stim/blank for whole chamber 
-function drawMimg(data, dynamicRange, frameRange)
+function drawMimg(data, dynamicRange, frameRange, bNormalizeTime)
     if nargin < 2
         dynamicRange = 2e-3;
     end
     if nargin < 3
         frameRange = 20:50;
     end
+    if nargin < 4
+        bNormalizeTime = 0;
+    end
     
     signal = relativeSignal(data.blank, data.stims, frameRange);
     meanSignal = mean(signal,3);
+    if bNormalizeTime
+        startFrame = 25;
+        msecPerFrame = 10;
+        times = msecPerFrame * (frameRange - startFrame);
+    else
+        times = frameRange;
+    end
     
     figure;
-    mimg(meanSignal,100,100,-dynamicRange,dynamicRange,frameRange); 
+    mimg(meanSignal,100,100,-dynamicRange,dynamicRange,times); 
     colormap(mapgeog);
 end
