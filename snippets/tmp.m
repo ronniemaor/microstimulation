8382705bb6c94c152b893730e35273b3471134ca
Ronnie Maor <ronnie.maor@gmail.com>
2013-05-04 16:18:45 +0300

    tmp - drawing illusration of fitting process

--- snippets/tmp.m
+++ snippets/tmp.m
@@ -1,11 +1,28 @@
 function tmp()
-    allSessions = getAllSessionConfigs();
-    for cSession = allSessions.keys()
-        sessionKey = cSession{1};
-        data = loadData(sessionKey);
-        data = findPeak(data);
-        dt = 10*(data.peakFrame-25);
-        fprintf('Peak for %s at frame %d (%.2f msec after stimulation)\n', sessionKey, data.peakFrame, dt)
-    end
+    data = loadData('J29c');
+    data = findPeak(data);
+    isVertical = 1;
+    fit = GaussianFit;
+    
+    figure
+    [distances, yAll, yFit] = doOneFit(data, data.peakFrame, isVertical, fit);
+    yscale = max(yAll);
+    xscale = max(distances);
+    yAll = yAll / yscale;
+    yFit = yFit / yscale;
+    distances = distances / xscale;
+    plot(distances, yAll, 'rx')
+    hold on
+    plot(distances,yFit,'b')
+end
 
+function [distances, yAll, yFit] = doOneFit(data, frame, isVertical, fit)
+    W = 9;
+    signal = relativeSignal(data.blank, data.stims, frame);
+    [eqMeans, ~, eqVals] = sliceStats(signal, data.mask, data.C, W, isVertical);
+    mmPerPixel = 0.1;
+    distances = eqVals * mmPerPixel; % convert to mm
+    yAll = mean(eqMeans,1);
+    P = fit.fitParams(distances,yAll);
+    yFit = fit.fitValues(distances,P);
 end
