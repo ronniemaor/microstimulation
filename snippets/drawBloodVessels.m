81da75b8a6e249bc5db3f829408af1d5b005514e
Ronnie Maor <ronnie.maor@gmail.com>
2013-06-24 22:45:23 +0300

    mark blood vessels detection on blood vessel image

--- snippets/drawBloodVessels.m
+++ snippets/drawBloodVessels.m
@@ -1,6 +1,10 @@
-function drawBloodVessels(data, bShowSlices)
-    if nargin < 2
+function drawBloodVessels(data, bShowSlices, bMarkVessels)
+    if ~exist('bShowSlices','var')
         bShowSlices = 1;
+    end
+    
+    if ~exist('bMarkVessels','var')
+        bMarkVessels = 0;
     end
     
     blVes = mean(data.rawBlank(:,2:100),2);
@@ -10,11 +14,27 @@ function drawBloodVessels(data, bShowSlices)
         markedRegion = markSlice(data);
         blVes(markedRegion) = min(blVes); % will show up in black
     end
+        
+    if bMarkVessels
+        markedRegion = markVessels(data);
+        blVes(markedRegion) = min(blVes); % will show up in black
+    end
     
     figure; 
     mimg(blVes,100,100,'auto',0,' '); 
     topLevelTitle(sprintf('%s - blood vessels', data.sessionKey));
     impixelinfo;
+end
+
+function markedRegion = markVessels(data)
+    % w = 2; p = 4; % J29
+    % w = 2; p = 15; % M18
+    % w = 1.7; p = 15; % M18-2
+    blVes = mean(data.rawBlank(:,2:100),2);
+    blVes = mfilt2(blVes,100,100,w,'hm');
+    chamber = blVes(data.origMask);
+    threshold = prctile(chamber,p);
+    markedRegion = blVes < threshold | ~data.origMask;
 end
 
 function markedRegion = markSlice(data)
