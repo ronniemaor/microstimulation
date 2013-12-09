c879b8d1c9317fc83173c6b0d4d602d0676e5c66
Ronnie Maor <ronnie.maor@gmail.com>
2013-12-09 13:18:58 +0200

    add allBlanks to data

--- snippets/preprocessAndSave.m
+++ snippets/preprocessAndSave.m
@@ -2,15 +2,16 @@ function preprocessAndSave(sessionKey)
     % read the raw files and do the preprocessing
     fprintf('\n**** Preprocessing session %s\n\n', sessionKey)
     dataDir = getSessionDataDir(sessionKey);
-    [blank, stims, rawBlank, mask] = preprocessSession(dataDir);
+    [blank, stims, rawBlank, mask, allBlanks] = preprocessSession(dataDir);
 
     % we're only interested in early times, so keep files small
     % by keeping only initial frames for the stimulus
     nFrames = 80;     
     stims = stims(:,1:nFrames,:);
+    allBlanks = allBlanks(:,1:nFrames,:);
     
     % save 
     filename = [dataDir,'/preprocessed'];
     fprintf('***** Saving preprocessed data to %s\n', filename)
-    save(filename, 'blank', 'stims', 'rawBlank', 'mask')
+    save(filename, 'blank', 'stims', 'rawBlank', 'mask', 'allBlanks')
 end
