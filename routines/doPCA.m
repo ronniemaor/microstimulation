155e0003dc715c09db30536e78e2e5d87685de1a
Ronnie Maor <ronnie.maor@gmail.com>
2014-01-19 23:07:04 +0200

    docs

--- routines/doPCA.m
+++ routines/doPCA.m
@@ -1,4 +1,13 @@
 function [V,d,C] = doPCA(X,k)
+    % Get first k PCs from X.
+    % Input:
+    %   X - An n x m matrix: n samples of vectors having m features each.
+    %   k - Number of PCs to return.
+    % Output:
+    %   V - The first k eigenvectors of the correlation matrix.
+    %       V is an n x k matrix. Column j is PC number j.
+    %   d - The first k eigenvalues.
+    %   C - The m x m correlation matrix
     X = X - repmat(mean(X),size(X,1),1);
     C = X'*X;
     [V,D] = eigs(C,k);
